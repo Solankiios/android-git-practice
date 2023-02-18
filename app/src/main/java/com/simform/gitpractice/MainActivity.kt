0cb8e0accab4b59d739edf5cb14a69e240efca40
SPCRi07 <solankiparth115@gmail.com>
2023-02-18 07:53:57 +0530

    - Login Code

--- app/src/main/java/com/simform/gitpractice/MainActivity.kt
+++ app/src/main/java/com/simform/gitpractice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.simform.gitpractice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    //Initialization
+    lateinit var email: EditText
+    lateinit var pass: EditText
+    lateinit var login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            //Login Code
+        email = findViewById(R.id.EmailAddressText)
+        pass = findViewById(R.id.passwordText)
+        login = findViewById(R.id.loginbtn)
     }
+
 }
